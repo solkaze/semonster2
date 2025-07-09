6e2ca0fe5345f3a3f6fca733cf1b7234eedf1df6
GenichiMaruo <85728541+GenichiMaruo@users.noreply.github.com>
2025-07-09 10:31:44 +0900

    Itemクラスに売却価格を取得するメソッドを追加し、ItemStackクラスを新規作成

---
+++ app/src/main/java/org/example/ItemStack.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class ItemStack {
+    public final Item item;
+    public int count;
+
+    public ItemStack(Item item, int count) {
+        this.item = item;
+        this.count = count;
+    }
+}
